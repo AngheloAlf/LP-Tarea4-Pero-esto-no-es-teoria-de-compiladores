1ae3fff225bd12083921e7c103b65096c71419d6
Alf <angheloalf95@gmail.com>
2016-01-02 03:07:35 -0300

    stdlib

---
+++ stdlib.py
@@ -0,0 +1,32 @@
+def EQ(A, B):
+	if A == B: return True
+	else: return False
+
+def NEQ(A, B):
+	return not EQ(A, B)
+
+def LT(A, B):
+	if A < B: return True
+	else: return False
+
+def GT(A, B):
+	return not LEQ(A, B)
+
+def LEQ(A, B):
+	if A <= B: return True
+	else: return False
+
+def GEQ(A, B):
+	return not LT(A, B)
+
+def ADD(A, B):
+	return A + B
+
+def SUB(A, B):
+	return A - B
+
+def INPUT():
+	return int(raw_input(""))
+
+def OUTPUT(OUT):
+	print OUT
