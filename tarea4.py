7563609f7b6d76dc5a6a8b9bb4510ecdbf4c9d1c
Alf <angheloalf95@gmail.com>
2016-01-03 00:34:48 -0300

    Asignacion de variables echa :)

--- tarea4.py
+++ tarea4.py
@@ -8,29 +8,44 @@ def getCommandName():
 	else:
 		return "alf.alf"
 
-def revisarAsignacion(i):
-	weaOriginal = i.strip()
-	asignacion = re.search('<=', weaOriginal)
-	if asignacion == None:
-		return
-	a = weaOriginal[:asignacion.span()[0]]
-	b = weaOriginal[asignacion.span()[1]:]
-	buscando = re.search("VAR", a)
-	c = a[buscando.span()[1]:]
-	variable = re.split("\(\s*(.*)\s*\)", c)[1]
-	d = re.sub("\s*","",b)
-	return variable+" = "+d
+def revisarAsignacion(leseraOriginal):
+	return re.search('<=', leseraOriginal) or re.search('=>', leseraOriginal)
+
+def generarAsignacion(leseraOriginal):
+	def asignacionAsignosa(variable, valor):
+		buscando = re.search("VAR", variable)
+		c = variable[buscando.span()[1]:]
+		variable = re.split("\(\s*(.*)\)", c)
+		variable = variable[1]
+		valor = re.sub("\s*","",valor)
+		return variable+" = "+valor
+	asignacionIzq = re.search('<=', leseraOriginal)
+	asignacionDer = re.search('=>', leseraOriginal)
+	if asignacionIzq:
+		variable = leseraOriginal[:asignacionIzq.span()[0]]
+		valor = leseraOriginal[asignacionIzq.span()[1]:]
+		return asignacionAsignosa(variable, valor)
+
+	if asignacionDer:
+		variable = leseraOriginal[asignacionDer.span()[1]:]
+		valor = leseraOriginal[:asignacionDer.span()[0]]
+		return asignacionAsignosa(variable, valor)
+
+	return None
+
+
 
 nombre = getCommandName()
 datosNuevos = list()
 datosNuevos.append("from stdlib import *\n")
 
-try:
-	archivo = open(nombre)
-	for i in archivo:
-		print revisarAsignacion(i)
-except:
-	raise
+
+archivo = open(nombre)
+for i in archivo:
+	if revisarAsignacion(i.strip()):
+		datosNuevos.append(generarAsignacion(i.strip())+"\n")
+
+print datosNuevos
 # except IOError, (errno, strerror) :
 # 	print "I/O error(%s) : %s " % ( errno , strerror)
 # except ValueError :
